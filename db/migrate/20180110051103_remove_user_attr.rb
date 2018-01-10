bd5988d5542147310d6aff2e5468d1adb3ac6566
Luke Cassar <lukecassar7@gmail.com>
2018-01-10 15:58:17 +1000

    clean up migrations and legacy user attr in fixture

--- db/migrate/20180110051103_remove_user_attr.rb
+++
@@ -1,7 +0,0 @@
-class RemoveUserAttr < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :current_speed, :integer
-    remove_column :users, :base_hidden, :boolean
-    remove_column :users, :paused, :boolean
-  end
-end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,12 +10,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'User associations' do
-    user = user_by_name('Luke')
-    call = 'user.enrolments.count'
-    Enrolment.first.destroy
-    assert_difference(call, 1, 'map did not save') do
-      user.enrolments.create(word_rank: 1, lang_map_id: 1)
-    end
   end
 
   test 'User.current_name' do
