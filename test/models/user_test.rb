41c4a841e060c3017ca087d0b2bf4fa39f423b8f
Luke Cassar <lukecassar7@gmail.com>
2018-11-19 19:34:43 +1000

    Remove useless variable in user test.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'create_student' do
     assert_difference('Student.count', 1) do
-      user = User.create(name: 'Luke')
+      User.create(name: 'Luke')
     end
   end
 end
