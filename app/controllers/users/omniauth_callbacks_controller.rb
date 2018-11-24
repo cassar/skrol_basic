4751b84dc055e6800f90357053f420e4f18e5a4a
Luke Cassar <lukecassar7@gmail.com>
2018-11-24 21:40:40 +1000

    Fix omniauth style cops.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env['omniauth.auth'])
+    handle_user_save
+  end
 
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def handle_user_save
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect @user, event: :authentication
@@ -10,9 +18,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
-  end
-
-  def failure
-    redirect_to root_path
   end
 end
