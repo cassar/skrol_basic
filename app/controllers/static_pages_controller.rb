7d933337da66e2fafa95073eb58a5f9a328c248e
Luke Cassar <lukecassar7@gmail.com>
2018-11-25 15:32:09 +1000

    Authenticate user before allowing to admin.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def landing; end
 
   def admin
+    authenticate_user!
     check_admin_privileges
     @user_count = User.count
     @course_count = Course.count
