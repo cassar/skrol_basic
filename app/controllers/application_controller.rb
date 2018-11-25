d52a7fb1d05925e5f16ccb94a81f7f0a7f1ff86f
Luke Cassar <lukecassar7@gmail.com>
2018-11-25 13:30:42 +1000

    Add admin boolean to model and enforce in controller and views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  # Makes sure a user is an admin before letting them proceed.
+  def check_admin_privileges
+    return if current_user.admin
+
+    flash[:alert] = 'You must have admin privileges to access this section.'
+    redirect_to root_path
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def landing; end
 
   def admin
+    check_admin_privileges
     @user_count = User.count
     @course_count = Course.count
   end
