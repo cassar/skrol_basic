0c3b1a8dcbfa1c07d29000b81fca23db0fb9fa0e
Luke Cassar <lukecassar7@gmail.com>
2018-12-09 10:58:22 +1000

    Fill out admin language map show and add preload functions.

--- app/controllers/admin/language_maps_controller.rb
+++ app/controllers/admin/language_maps_controller.rb
@@ -3,14 +3,13 @@ class Admin::LanguageMapsController < ApplicationController
   before_action :check_admin_privileges
 
   def index
-    @language_maps = LanguageMap.all
-    @id_languages = {}
-    Language.all.each { |l| @id_languages[l.id] = l }
+    @language_maps = LanguageMap.all.preload(:base_language, :target_language)
   end
 
   def show
     @language_map = LanguageMap.find(params[:id])
     @base_language = @language_map.base_language
     @target_language = @language_map.target_language
+    @courses = @language_map.courses.preload(:enrolments, :word_scores, :sentence_scores)
   end
 end
